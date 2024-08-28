55f923d50a973bd34e75376f96491a73b707a1f6
Marc Andreu <marc@itqualab.com>
2024-08-28 08:14:23 +0000

    up to attriute binging

--- Tutorial.md
+++ Tutorial.md
@@ -1,7 +1,9 @@
 # Status:
 
-* Continue at min 1,
+* Continue at min 16:50,
 
  [Vue JS 3 Tutorial for Beginners #3 - Vue.js Basics (part 2)
 ](https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3)
+
+
 
--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const app = Vue.createApp({
     data(){
         return{
+            url: 'https://www.itqualab.com',
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true},
+            ]
         }
     },
     methods: {
@@ -13,7 +21,14 @@ const app = Vue.createApp({
         },
         toggleShowBooks(){
             this.showBooks = !this.showBooks;
-        }
+        },
+        handleEvent(event, dataParam2){
+            console.log("Event triggered", event, event.type, dataParam2);
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        }   
     },
 });
 
